31e44b2016eb70de72cf821f721853361b8c8606
loglob <23386237+loglob@users.noreply.github.com>
2022-04-25 16:29:03 +0200

    Removed Discore dependency

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ dotnet publish --self-contained -c Release -r linux-x64 -p:PublishSingleFile=tru
 
 if [ "$serviceStatus" = "active" ]; then systemctl stop scpbot; fi;
 
-cp -r bin/Release/net6/linux-x64/publish/* /srv/scpbot
+cp -r bin/Release/net6.0/linux-x64/publish/* /srv/scpbot
 cp config.json token /srv/scpbot
 
 if [ "$serviceStatus" = "active" ]; then systemctl start scpbot; fi;
