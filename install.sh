900d8a74debba280768eaa0a62fd3797293b1a13
loglob <23386237+loglob@users.noreply.github.com>
2023-01-22 16:33:49 +0100

    tweaked install script  don't overwrite existing config

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
+DEST="/srv/scpbot"
+
 serviceStatus=$(systemctl is-active scpbot)
-mkdir -p /srv/scpbot
+mkdir -p "$DEST"
 dotnet publish --self-contained -c Release -r linux-x64 -p:PublishSingleFile=true
 
 if [ "$serviceStatus" = "active" ]; then systemctl stop scpbot; fi;
 
-cp -r bin/Release/net7.0/linux-x64/publish/* /srv/scpbot
-cp config.json token /srv/scpbot
+cp -r bin/Release/net7.0/linux-x64/publish/* "$DEST"
+cp -n config.json token "$DEST"
 
 if [ "$serviceStatus" = "active" ]; then systemctl start scpbot; fi;
